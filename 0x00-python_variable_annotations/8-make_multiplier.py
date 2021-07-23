15d63e192d7cbe4b0bd39c972500b5dfb84fd5c0
Huy Nguyen <huy.nguyen@holbertonschool.com>
2021-07-23 08:54:13 +0000

    fix 8 & 9

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Takes a float multiplier, returns a function that multiplies a float by multiplier """
+""" Takes a float multiplier,
+returns a function that multiplies a float by multiplier """
 
 from typing import Callable
 
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"" Annotates function params and return values """
+""" Annotates the below function’s parameters """
 
 from typing import List, Tuple, Sequence, Iterable
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """ Duck-typing """
+    """ Duck typing """
     return [(i, len(i)) for i in lst]
