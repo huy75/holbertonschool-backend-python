68512ed75b47f447485681ef24146b1aed01d3dd
Huy Nguyen <huy.nguyen@holbertonschool.com>
2021-08-16 13:41:23 +0000

    feat task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -49,3 +49,22 @@ class TestGetJson(unittest.TestCase):
             self.assertEqual(real_response, test_payload)
             # check that mocked method called once per input
             mock_response.json.assert_called_once()
+
+
+class TestMemoize(unittest.TestCase):
+    """ Class for testing memoization """
+
+    def test_memoize(self):
+        """ Tests memoize function """
+
+        class TestClass:
+            """ Test class """
+
+            def a_method(self):
+                """ Method to always return 42 """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """ Returns memoized property """
+                return self.a_method()
