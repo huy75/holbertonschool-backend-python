13f1028b22b9986368618dddbdcae2d8c9b635de
Huy Nguyen <huy.nguyen@holbertonschool.com>
2021-08-16 13:44:22 +0000

    fix add patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -68,3 +68,11 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 """ Returns memoized property """
                 return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as patched:
+            test_class = TestClass()
+            real_return = test_class.a_property
+            real_return = test_class.a_property
+
+            self.assertEqual(real_return, 42)
+            patched.assert_called_once()
